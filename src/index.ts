f163fe031cfd67114eda0ab023246301b9a8b4b6
Tommy Toban <tommy.deppatori@gmail.com>
2020-03-25 08:57:30 +0700

    firs init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {HcosystemBackendApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {HcosystemBackendApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new HcosystemBackendApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
